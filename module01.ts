ef7fbe650d4674593f7a05fb96431cfc5d332eef
RodriMR <rodrimr2008@gmail.com>
2022-11-25 15:26:35 -0300

    Module 2 type inference

--- module01.ts
+++ module01.ts
@@ -2,3 +2,7 @@ function addNumbers(x: number, y: number) {
   return x + y;
 }
 console.log(addNumbers(3, 6));
+
+let x: number; //* Explicitly declares x as a number type
+let y = 1; //* Implicitly declares y as a number type
+let z; //* Declares z without initializing it
