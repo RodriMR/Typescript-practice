d043f47298d230fabe67414b462a38f1726e0087
RodriMR <rodrimr2008@gmail.com>
2022-11-25 15:36:14 -0300

    Module 3 Primitive types

--- module01.ts
+++ module01.ts
@@ -3,20 +3,38 @@ function addNumbers(x: number, y: number) {
 }
 console.log(addNumbers(3, 6));
 
-let x: number; //* Explicitly declares x as a number type
-let y = 1; //* Implicitly declares y as a number type
-let z; //* Declares z without initializing it
+// let x: number; //* Explicitly declares x as a number type
+// let y = 1; //* Implicitly declares y as a number type
+// let z; //* Declares z without initializing it
 
-x = 1;
-x = "one";
+// x = 1;
+// x = "one";
 
-//Intelisense shows NUMBER methods
-y.toExponential;
-y.toFixed;
-y.toLocaleString;
-y.toPrecision;
-y.toString;
-y.valueOf;
+// //Intelisense shows NUMBER methods
+// y.toExponential;
+// y.toFixed;
+// y.toLocaleString;
+// y.toPrecision;
+// y.toString;
+// y.valueOf;
 
-z = 1;
-z = "one";
+// z = 1;
+// z = "one";
+
+//Number and BigInteger types
+
+// let x: number;
+// let y = 0;
+// let z: number = 123.456;
+// let big: bigint = 100n;
+
+//String type
+
+// let s: string;
+// let empty = "";
+// let abc = 'abc';
+
+// let firstName: string = "Mateo";
+// let sentence: string = `My name is ${firstName}.
+//     I am new to TypeScript.`;
+// console.log(sentence);
