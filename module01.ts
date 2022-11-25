52d4bc2aada32bed8530b4e3329eda66307a0ce6
RodriMR <rodrimr2008@gmail.com>
2022-11-25 15:26:42 -0300

    Module 2 type inference

--- module01.ts
+++ module01.ts
@@ -6,3 +6,17 @@ console.log(addNumbers(3, 6));
 let x: number; //* Explicitly declares x as a number type
 let y = 1; //* Implicitly declares y as a number type
 let z; //* Declares z without initializing it
+
+x = 1;
+x = "one";
+
+//Intelisense shows NUMBER methods
+y.toExponential;
+y.toFixed;
+y.toLocaleString;
+y.toPrecision;
+y.toString;
+y.valueOf;
+
+z = 1;
+z = "one";
